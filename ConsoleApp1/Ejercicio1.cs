10998119f9d4fd0ec0e93f5359ea5ded00e77067
Noelia Soledad Cordoba <noelia.cordoba@neoris.cxnetworks.net>
2018-05-15 09:54:05 -0300

    First Commit

---
+++ ConsoleApp1/Ejercicio1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Ejercicio1
+    {
+        int n, m;
+        int[,] board; // Fills with 0 and 1, white represented by 1 and black by 0 
+
+        public Ejercicio1()
+        {
+        }
+
+        public bool IsWhite(int row, int col)
+        {
+            return board[row,col].Equals(0);
+                 
+        }
+    }
+}
---
+++ ConsoleApp1/Ejercicio2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Ejercicio2
+    {
+        public bool IsAnagram(string str1, string str2)
+        {
+            if (str1.Length != str2.Length)
+                return false;
+
+            if (str1 == str2)
+                return false;
+
+            char[] array1 = str1.ToCharArray();
+            char[] array2 = str2.ToCharArray();
+            int n = 0;
+            int count1 = 0;
+            int count2 = 0;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                char l = array1[i];
+                n = 0;
+
+                while (n < array1.Length)
+                {
+                    if (array1[n] == l)
+                        count1++;
+
+                    if (array2[n] == l)
+                        count2++;
+
+                    n++;
+                }
+
+                if (count1 != count2)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
